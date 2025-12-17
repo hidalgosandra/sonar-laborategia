edf48a88ac3239e5a36d53c735561088d0dd028e
Sandra Hidalgo <hidalgo.sandra@uni.eus>
2025-12-17 10:46:16 +0100

    index php added

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Adibide sinplea SonarQube bidez analizatzeko.
+ * Kode honek nahita egindako "akats" txikiak ditu.
+ */
+
+$izena = "Ikaslea";
+
+// 1. Arazoa: Baldintza hau beti da egia (Redundant code)
+if ($izena == "Ikaslea") {
+    echo "Kaixo, " . $izena . "!";
+}
+
+// 2. Arazoa: Aldagai hau definitu da baina ez da inoiz erabiltzen (Unused variable)
+$zenbakiSekretua = 12345;
+
+function gehiketa($a, $b)
+{
+    return $a + $b;
+}
+
+// Funtzioa deitzen dugu, baina ez dugu emaitza erabiltzen
+gehiketa(5, 10);
